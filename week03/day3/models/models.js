1ba175b5e1c61c80756fa5b0fc6417cd5f3dc69a
Kitan Garcia <kitangar@gmail.com>
2018-07-11 13:15:26 -0700

    finish mongo and layout/partial examples

--- week03/day3/models/models.js
+++ week03/day3/models/models.js
@@ -9,7 +9,78 @@ if (! process.env.MONGODB_URI) {
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var Cat; // YOUR CODE HERE - define the cat model
+var Cat = mongoose.model("Cat",
+{
+  name: String,
+  furcolor: String
+}); // YOUR CODE HERE - define the cat model
+
+
+var crooks = new Cat(
+{
+  name: "Crookshanks",
+  furcolor: "Black"
+}); // YOUR CODE HERE - define the cat model
+
+crooks.save(function(err)
+{
+  if (err)
+  {
+    console.log("failed", err);
+  }
+  else
+  {
+    console.log("saved");
+  }
+});
+
+var biggles =new Cat(
+{
+  name: "Mr. Bigglesworth",
+  furcolor: "White"
+}); // YOUR CODE HERE - define the cat model
+
+biggles.save(function(err)
+{
+  if (err)
+  {
+    console.log("failed", err);
+  }
+  else
+  {
+    console.log("saved");
+  }
+});
+
+var empur = new Cat(
+{
+  name: "Empurress",
+  furcolor: "Calico"
+}); // YOUR CODE HERE - define the cat model
+
+empur.save(function(err)
+{
+  if (err)
+  {
+    console.log("failed", err);
+  }
+  else
+  {
+    console.log("saved");
+  }
+});
+
+Cat.findOne(function(error, cats)
+{
+  if (error)
+  {
+    console.log("can't find cat", error);
+  }
+  else
+  {
+    console.log("found cat", cats);
+  }
+});
 
 Cat.find(function(error, cats) {
   if (error) {
