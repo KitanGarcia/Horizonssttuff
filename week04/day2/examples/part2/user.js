5f8a390dbd2631c52f1547c6b9d832b7e47a8423
Kitan Garcia <kitangar@gmail.com>
2018-07-17 10:51:31 -0700

    finish virtual example (part2sec2)

--- week04/day2/examples/part2/user.js
+++ week04/day2/examples/part2/user.js
@@ -16,6 +16,20 @@ var userSchema = new Schema({
   }
 });
 
+var ageVirtual = userSchema.virtual("age");
+ageVirtual.get(function()
+{
+
+  console.log(new Date);
+  var age = new Date() - this.birthday;
+  var seconds = (age/1000);
+  var minutes = (seconds/60);
+  var hours = (minutes/60);
+  var days = hours/24;
+  var years = days/365;
+  return (Math.floor(years));
+})
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
