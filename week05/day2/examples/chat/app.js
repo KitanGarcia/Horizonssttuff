2d61e3c892af7a0382d98497d38ee189be14b25b
Kitan Garcia <kitangar@gmail.com>
2018-07-24 11:25:03 -0700

    finish chat

--- week05/day2/examples/chat/app.js
+++ week05/day2/examples/chat/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require("http").Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -17,11 +19,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Logging
 app.use(morgan('combined'));
 
+io.on("connection", function(socket)
+{
+  var user = "";
+  socket.on("username", function(username)
+  {
+    user = username;
+    socket.broadcast.emit("joinedRoom", username);
+    socket.emit("welcome", username);
+  });
+
+  socket.on("message", function(message)
+  {
+    console.log(message);
+    io.emit("serverMessage", user + ": " + message);
+  });
+});
+
+
 app.get('/', function(req, res) {
   res.render('index');
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
